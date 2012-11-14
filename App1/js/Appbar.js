72b6773b2769a34474197d6f81fe833c445387e5
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2012-11-14 02:29:34 -0600

    removed jquery and added save abilitiy
    also submitted app to store for approval

--- App1/js/Appbar.js
+++ App1/js/Appbar.js
@@ -2,51 +2,97 @@
     "use strict";
     var page = WinJS.UI.Pages.define("default.html", {
         ready: function (element, options) {
-            document.getElementById("cmdHome")
-                .addEventListener("click", doClickHome, false);
             document.getElementById("cmdSave")
                 .addEventListener("click", doClickSave, false);
-            document.getElementById("cmdAdd")
-                .addEventListener("click", doClickAdd, false);
-            document.getElementById("cmdMail")
-                .addEventListener("click", doClickMail, false);
             document.getElementById("cmdDelete")
                 .addEventListener("click", doClickDelete, false);
-            /*WinJS.log && WinJS.log("To show the bar, swipe up from " +
-                "the bottom of the screen, right-click, or " +
-                "press Windows Logo + z. To dismiss the bar, " +
-                "tap in the application, swipe, right-click, " +
-                "or press Windows Logo + z again.", "sample", "status");*/
         },
     });
 
     // Command button functions
-    function doClickHome() {
-        //WinJS.log && WinJS.log("Home button pressed");
-    }
-
     function doClickSave() {
         //WinJS.log && WinJS.log("Save button pressed");
-    }
+        // Clean scenario output
+        //WinJS.log && WinJS.log("", "sample", "status");
 
-    function doClickAdd() {
-        //WinJS.log && WinJS.log("Add button pressed");
-    }
+        // Verify that we are currently not snapped, or that we can unsnap to open the picker
+        var currentState = Windows.UI.ViewManagement.ApplicationView.value;
+        if (currentState === Windows.UI.ViewManagement.ApplicationViewState.snapped &&
+            !Windows.UI.ViewManagement.ApplicationView.tryUnsnap()) {
+            // Fail silently if we can't unsnap
+            return;
+        }
 
-    function doClickMail() {
-        //WinJS.log && WinJS.log("Mail button pressed");
+        // Create the picker object and set options
+        var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+        savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+        // Dropdown of file types the user can save the file as
+        savePicker.fileTypeChoices.insert("HTML Document", [".html"]);
+        // Default file name if the user does not type one in or select a file to replace
+        savePicker.suggestedFileName = "markdown.html";
+
+        savePicker.pickSaveFileAsync().then(function (file) {
+            if (file) {
+                // Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.deferUpdates(file);
+                // write to file
+
+                //Windows.Storage.FileIO.writeTextAsync(file, file.name).done(function () {
+                var fileout = document.getElementById('right_col_inner').innerHTML;
+
+                //Windows.Storage.FileIO.writeTextAsync(file, $("#right_col_inner").html()).done(function () {
+                Windows.Storage.FileIO.writeTextAsync(file, fileout).done(function () {
+                    // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
+                    // Completing updates may require Windows to ask for user input.
+                    Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
+                        if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
+                            //WinJS.log && WinJS.log("File " + file.name + " was saved.", "sample", "status");
+                            // Saved
+                            var msg = new Windows.UI.Popups.MessageDialog(
+                                        "File " + file.name + " successfully saved.");
+
+                            // Add commands and set their command handlers
+                            msg.commands.append(new Windows.UI.Popups.UICommand("Close"));
+
+                            // Set the command that will be invoked by default
+                            msg.defaultCommandIndex = 0;
+
+                            // Set the command to be invoked when escape is pressed
+                            msg.cancelCommandIndex = 1;
+
+                            // Show the message dialog
+                            msg.showAsync();
+                        } else {
+                            //WinJS.log && WinJS.log("File " + file.name + " couldn't be saved.", "sample", "status");
+                            // Not saved
+                            var msg = new Windows.UI.Popups.MessageDialog(
+                                        "File " + file.name + " not saved.");
+
+                            // Add commands and set their command handlers
+                            msg.commands.append(new Windows.UI.Popups.UICommand("Close"));
+
+                            // Set the command that will be invoked by default
+                            msg.defaultCommandIndex = 0;
+
+                            // Set the command to be invoked when escape is pressed
+                            msg.cancelCommandIndex = 1;
+
+                            // Show the message dialog
+                            msg.showAsync();
+                        }
+                    });
+                });
+            } else {
+                // Operation Cancelled
+            }
+        });
     }
 
     function doClickDelete() {
         //WinJS.log && WinJS.log("Delete button pressed");
-        $("#left_col_inner").html("");
-        $("#right_col_inner").html("");
+        //$("#left_col_inner").html("");
+        //$("#right_col_inner").html("");
+        document.getElementById('left_col_inner').innerHTML = "";
+        document.getElementById('right_col_inner').innerHTML = "";
     }
-
-    WinJS.log = function (message) {
-        var statusDiv = document.getElementById("statusMessage");
-        if (statusDiv) {
-            statusDiv.innerText = message;
-        }
-    };
 })();
--- App1/js/default.js
+++ App1/js/default.js
@@ -1,6 +1,4 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
-(function () {
+﻿(function () {
     "use strict";
 
     WinJS.Binding.optimizeBindingReferences = true;
@@ -19,22 +17,8 @@
             }
             args.setPromise(WinJS.UI.processAll());
 
-            // Retrieve the button and register our event handler
-            //var helloButton = document.getElementById("helloButton");
-            //var bodyClick = document.getElementById("body");
-            //bodyClick.addEventListener("click", buttonFocus, false);
-            //bodyClick.addEventListener("keydown", buttonFocus, false);
-            //document.addEventListener("click", buttonFocus, false);
-            //document.addEventListener("keydown", buttonFocus, false);
-
-
             var helloButton = document.getElementById("left_col_inner");
             helloButton.addEventListener("keyup", buttonClickHandler, false);
-
-            //helloButton.addEventListener("keydown", buttonClickHandler, false);
-
-            //var helloButton = document.getElementById("convert");
-            //helloButton.addEventListener("click", buttonClickHandler, false);
         }
     };
 
@@ -47,11 +31,7 @@
         // args.setPromise().
     };
 
-    function buttonFocus(eventInfo) {
-        $("#left_col_inner").focus();
-    }
-
-    var ulist = false;
+    //var ulist = false;
    
     function textFormat(text) {
         while (text.indexOf("*") >= 0) {
@@ -88,24 +68,11 @@
     }
 
     function buttonClickHandler(eventInfo) {
-        // Currently Supported
-        // Headings
-        // Regular Text
-        // Unordered List
-        // Bold => *bold*
-        // Italics => _italics_
+        //$("#right_col_inner").text("");
+        document.getElementById('right_col_inner').innerHTML = "";
 
-        //var userName = document.getElementById("nameInput").value;
-        //var userName = $("#nameInput").text();
-        //var greetingString = "Hello, " + userName + "!";
-        //document.getElementById("greetingOutput").innerText = greetingString;
-        //$("#greetingOutput").text(greetingString);
-        //$("#right_col_inner").text($("#left_col_inner").text());
-
-        $("#right_col_inner").text("");
-
-        var inner = $("#left_col_inner").html();
-        //var inner = 'This is a test! What will happen!';
+        //var inner = $("#left_col_inner").html();
+        var inner = document.getElementById('left_col_inner').innerHTML;
 
         inner = inner.replace(/(&nbsp;)*/g, "");
 
@@ -122,10 +89,10 @@
 
             // This is for headers
             if ((j.charAt(index) == "#") == true) {
-                if (ulist == true) {
+                /*if (ulist == true) {
                     $("#right_col_inner").append("</ul>");
                     ulist = false;
-                }
+                }*/
 
                 while ((j.charAt(index) == "#") == true) {
                     index += 1;
@@ -134,8 +101,6 @@
                 var p;
 
                 // remove #
-                //j = j.replace(/#/g, '');
-
                 while ((j.charAt(0) == "#") == true) {
                     j = j.substring(1);
                 }
@@ -145,86 +110,78 @@
                 // add specific header
                 switch (index) {
                     case 1:
-                        p = $("<h1>").html(j);
-                        p.append("</h1>");
+                        //p = $("<h1>").html(j);
+                        //p.append("</h1>");
+                        p = "<h1>" + j + "</h1>";
                         break;
                     case 2:
-                        p = $("<h2>").html(j);
-                        p.append("</h2>");
+                        //p = $("<h2>").html(j);
+                        //p.append("</h2>");
+                        p = "<h2>" + j + "</h2>";
                         break;
                     case 3:
-                        p = $("<h3>").html(j);
-                        p.append("</h3>");
+                        //p = $("<h3>").html(j);
+                        //p.append("</h3>");
+                        p = "<h3>" + j + "</h3>";
                         break;
                     case 4:
-                        p = $("<h4>").html(j);
-                        p.append("</h4>");
+                        //p = $("<h4>").html(j);
+                        //p.append("</h4>");
+                        p = "<h4>" + j + "</h4>";
                         break;
                     case 5:
-                        p = $("<h5>").html(j);
-                        p.append("</h5>");
+                        //p = $("<h5>").html(j);
+                        //p.append("</h5>");
+                        p = "<h5>" + j + "</h5>";
                         break;
                     case 6:
-                        p = $("<h6>").html(j);
-                        p.append("</h6>");
+                        //p = $("<h6>").html(j);
+                        //p.append("</h6>");
+                        p = "<h6>" + j + "</h6>";
                         break;
                     default:
                         break;
                 }
-                $("#right_col_inner").append(p);
+                //$("#right_col_inner").append(p);
+                document.getElementById('right_col_inner').innerHTML += p;
             } else if ((j.charAt(index) == "+" || j.charAt(index) == "-") == true) {
                 // We have an unordered list
 
                 j = j.replace(/\+/g, '');
                 j = j.replace(/-/g, '');
 
-                if (ulist == false) {
+                /*if (ulist == false) {
                     $("#right_col_inner").append("<ul>");
                     ulist = true;
-                }
-
-                var p = $("<li>").html(textFormat(j));
-                p.append("</li>");
-                $("#right_col_inner").append(p);
-
-            } else {
-                if (ulist == true) {
-                    $("#right_col_inner").append("</ul>");
-                    ulist = false;
-                }
-
-                // This is for regular text
-                var p = $("<p>");
-
-                //var words = j.split(" ");
-                /*var w = j;
-
-                while (w.indexOf("*") >= 0) {
-                    // deal with bold
-                    w = w.replace("*", "<strong>");
-
-                    if (w.indexOf("*") < 0) {
-                        w += "</strong>";
-                    } else {
-                        w = w.replace("*", "</strong>");
-                    }
-                }
-                while (w.indexOf("_") >= 0) {
-                    // deal with italics
-                    w = w.replace("_", "<em>");
-
-                    if (w.indexOf("_") < 0) {
-                        w += "</em>";
-                    } else {
-                        w = w.replace("_", "</em>");
-                    }
                 }*/
 
+                //var p = $("<li>").html(textFormat(j));
+                //p.append("</li>");
+                //$("#right_col_inner").append(p);
+                var p = "<li>";
+                p += textFormat(j);
+                p += "</li>";
+                document.getElementById('right_col_inner').innerHTML += p;
+
+            } else {
+                /*if (ulist == true) {
+                    $("#right_col_inner").append("</ul>");
+                    ulist = false;
+                }*/
+
+                // This is for regular text
+                /*var p = $("<p>");
+
                 p.append(textFormat(j));
-                p.append("</p>");
+                p.append("</p>");*/
+
+                var p = "<p>";
+                p += textFormat(j);
+                p += "</p>";
 
 
-                $("#right_col_inner").append(p);
+                //$("#right_col_inner").append(p);
+                document.getElementById('right_col_inner').innerHTML += p;
             }
         }
     }
